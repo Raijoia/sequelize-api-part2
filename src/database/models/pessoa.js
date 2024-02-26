45c0c54e0f0830ca6f1b7f7ce9897bbee9d0d4c7
Raí Joia <megac154@gmail.com>
2024-02-26 18:43:39 -0300

    feat: add validator para cpf e nome para cadastro de pessoas

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,54 +1,72 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const isValid = require('../../utils/validatorCpfHelper');
+
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
     static associate(models) {
       Pessoa.hasMany(models.Curso, {
-        foreignKey: 'docente_id'
+        foreignKey: 'docente_id',
       });
       Pessoa.hasMany(models.Matricula, {
         foreignKey: 'estudante_id',
         // scope: { status: 'matriculado' },
-        as: 'aulasMatriculadas'
+        as: 'aulasMatriculadas',
       });
     }
   }
-  Pessoa.init({
-    nome: DataTypes.STRING,
-    email: { 
-      type:DataTypes.STRING,
-      validate:{
-        isEmail: {
-          args: true,
-          msg: 'O campo de email deve ser um email válido'
-        }
-      }
-    },
-    cpf: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoa',
-    tableName: 'pessoas',
-    paranoid: true,
-    defaultScope: {
-      where: {
-        ativo: true,
-      }
-    },
-    scopes: {
-      todosOsRegistros: {
-        where: {}
+  Pessoa.init(
+    {
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
       },
-      desativados: {
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'O campo de email deve ser um email válido',
+          },
+        },
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        validate: {
+          validatorCPF: (cpf) => {
+            if (!isValid(cpf)) throw new Error('CPF inválido');
+          },
+        },
+      },
+      ativo: DataTypes.BOOLEAN,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Pessoa',
+      tableName: 'pessoas',
+      paranoid: true,
+      defaultScope: {
         where: {
-          ativo: false,
-        }
-      }
+          ativo: true,
+        },
+      },
+      scopes: {
+        todosOsRegistros: {
+          where: {},
+        },
+        desativados: {
+          where: {
+            ativo: false,
+          },
+        },
+      },
     }
-  });
+  );
   return Pessoa;
 };
